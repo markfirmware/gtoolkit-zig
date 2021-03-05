444f4c220302189e622071ec11c7a87306acad93
markfirmware <mark.kendrat@gmail.com>
2021-03-05 02:51:42 +0000

    json: bool

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,15 @@ const std = @import("std");
 const parse = std.zig.parse;
 
 pub fn main() anyerror!void {
+    try do(false);
+    try do(true);
+}
+
+fn do(json: bool) anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     var alloc = &arena.allocator;
-    var tree = try parse(alloc, "fn function () void {}");
+    var tree = try parse(json, alloc, "pub inline fn function () void {}");
     var rendering = try tree.render(alloc);
     std.log.info("{s}", .{rendering});
 }
