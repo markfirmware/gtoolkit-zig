94a6d4798e1867284008ba2ecce8257df6b6ae25
markfirmware <mark.kendrat@gmail.com>
2021-03-06 01:10:37 +0000

    .

--- src/main.zig
+++ src/main.zig
@@ -1,37 +1,67 @@
-const std = @import("std");
-const parse = std.zig.parse;
-const ast = std.zig.ast;
 
-pub fn main() anyerror!void {
-    var j: JsonRenderer = undefined;
-    try j.render("fn function () void {}");
-}
-
-const JsonRenderer = struct {
-    tree: ast.Tree,
-    const Self = @This();
-    fn render(self: *Self, source: []const u8) anyerror!void {
-        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-        defer arena.deinit();
-        var alloc = &arena.allocator;
-        self.tree = try parse(alloc, "fn function () void {}");
-        // var rendering = try tree.render(alloc);
-        var members: []const ast.Node.Index = self.tree.rootDecls();
-        self.add(members[0]);
-    }
-    fn openD(self: *Self, tag: ast.Node.Tag) void {
-        std.log.info("{{tag: {s}", .{@tagName(tag)});
-    }
-    fn closeD(self: *Self) void {
-        std.log.info("}}", .{});
-    }
-    fn add(self: *Self, decl: ast.Node.Index) void {
-        var tag = self.tree.nodes.items(.tag)[decl];
-        if (tag == .fn_decl) {
-            self.openD(tag);
-            self.closeD();
-        } else {
-            std.log.info("tag {s} not yet processed", .{@tagName(tag)});
+    pub fn main() !void {
+        const allocator = &std.heap.FixedBufferAllocator.init(buf[0 ..]).allocator;
+        const tree = try zig.parse(allocator, source);
+        var i: u32 = 0;
+        while (tree.root_node.iterate(i)) |trunk| : (i += 1) {
+            findStruct(tree, 0, i, trunk);
         }
     }
-};
+ 
+    fn findStruct(tree: *Tree, depth: u32, n: u32, trunk: *Node) void {
+        if (trunk.cast(VarDecl)) |var_decl| {
+             if (trunk.iterate(0)) |container_node| {
+                 if (container_node.cast(ContainerDecl)) |container_decl| {
+                      if (mem.eql(u8, tree.tokenSlice(container_decl.kind_token), "struct")) {
+                          const struct_name = tree.tokenSlice(var_decl.name_token);
+                          var has_field = false;
+                          var has_state = false;
+                          var i: u32 = 0;
+                          while (container_node.iterate(i)) |node| : (i += 1) {
+                              if (node.cast(VarDecl)) |state_decl| {
+                                  if (mem.eql(u8, tree.tokenSlice(state_decl.mut_token), "var")) {
+                                      has_state = true;
+                                  }
+                              } else if (node.cast(ContainerField)) |container_field| {
+                                  has_field = true;
+                              }
+                          }
+                          if (has_state and !has_field) {
+                              var d: u32 = 0;
+                              while (d < depth) : (d += 1) {
+                                  warn("    ", .{});
+                              }
+                              warn("stateful singleton {}\n", .{struct_name});
+                          }
+                          i = 0;
+                          while (container_node.iterate(i)) |node| : (i += 1) {
+                              if (node.cast(VarDecl)) |state_decl| {
+                                  if (!mem.eql(u8, tree.tokenSlice(state_decl.mut_token), "var")) {
+                                      findStruct(tree, depth + 1, i, node);
+                                  }
+                              }
+                          }
+                      }
+                  }
+             }
+        } else {
+            var i: u32 = 0;
+            while (trunk.iterate(i)) |node| : (i += 1) {
+                findStruct(tree, depth + 1, i, node);
+            }
+        }
+    }
+
+    const ast = zig.ast;
+    const ContainerDecl = ast.Node.ContainerDecl;
+    const ContainerField = ast.Node.ContainerField;
+    const mem = std.mem;
+    const Node = ast.Node;
+    const source = @embedFile("main.zig");
+    const std = @import("std");
+    const Tree = ast.Tree;
+    const VarDecl = ast.Node.VarDecl;
+    const warn = std.debug.warn;
+    const zig = std.zig;
+
+    var buf:[1024 * 1024] u8 = undefined;
