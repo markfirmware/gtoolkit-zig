8a48aa9497b680e2d03d16a518fbc97968073355
markfirmware <mark.kendrat@gmail.com>
2021-03-06 20:31:23 +0000

    Tree tokens

--- src/main.zig
+++ src/main.zig
@@ -5,51 +5,28 @@ pub fn main() !void {
     while (i < tree.nodes.len) : (i += 1) {
         var tag = tree.nodes.items(.tag)[i];
         switch (tag) {
-            .address_of,
-            .array_access,
-            .array_init_dot_two,
-            .array_type,
-            .assign_add,
-            .block,
-            .block_two,
             .block_two_semicolon,
             .builtin_call_two,
             .call,
-            .call_one,
+            .error_union,
             .field_access,
             .fn_decl,
-            .if_simple,
+            .identifier,
             .root,
-            .slice_open,
-            .switch,
-            .switch_case,
-            .switch_case_one,
-            .try,
-            .while_cont => {
-                info("incomplete {} {s}", .{tag, tree.tokenSlice(tree.firstToken(i))});
-            },
-            .simple_var_decl => {
-                info("incomplete {} {s}", .{tag, tree.tokenSlice(tree.firstToken(i))});
+            .simple_var_decl,
+            .struct_init_dot_two => {
+                info("tbd {} {s}", .{tag, tree.tokenSlice(tree.firstToken(i))});
             },
             .fn_proto_simple => {
+                info("incomplete {} {s}", .{tag, tree.tokenSlice(tree.firstToken(i))});
                 var buffer: [1]Node.Index = undefined;
                 var f = tree.fnProtoSimple(&buffer, i);
                 if (f.name_token) | name_token | {
-                    info("incomplete {} {s}", .{tag, tree.tokenSlice(name_token)});
+                    info("incomplete {} name={s}", .{tag, tree.tokenSlice(name_token)});
                 }
             },
-            .identifier => {
-                info("incomplete {} {s}", .{tag, tree.tokenSlice(tree.firstToken(i))});
-            },
-            .less_than,
-            .mul => {
-                info("incomplete {} {s}", .{tag, tree.tokenSlice(tree.firstToken(i))});
-            },
-            .enum_literal,
-            .integer_literal,
-            .string_literal,
-            .undefined_literal => {
-                info("processed {} {s}", .{tag, tree.tokenSlice(tree.firstToken(i))});
+            .string_literal => {
+                info("complete {} {s}", .{tag, tree.tokenSlice(tree.firstToken(i))});
             },
             else =>{
                 info("else {}", .{tag});
@@ -64,7 +41,7 @@ const ContainerField = ast.Node.ContainerField;
 const info = std.log.info;
 const mem = std.mem;
 const Node = ast.Node;
-const source = @embedFile("example1.zig");
+const source = @embedFile("example1/src/main.zig");
 const std = @import("std");
 const Tree = ast.Tree;
 const VarDecl = ast.Node.VarDecl;
